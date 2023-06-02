b27fb31b7daeb78db59df6615db401ea3923ab0a
wael-morjen <waelmorjen@gmail.com>
2023-06-02 18:02:42 +0100

    listing creation

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -2,13 +2,13 @@
 
 interface HeadingProps {
     title?: string;
-    subTitle?: string;
+    subtitle?: string;
     center?: boolean
 }
 
 const Heading: React.FC<HeadingProps> = ({
     title,
-    subTitle,
+    subtitle,
     center
 }) => {
     return (
@@ -17,7 +17,7 @@ const Heading: React.FC<HeadingProps> = ({
                 {title}
             </div>
             <div className="font-light text-neutral-500 mt-2">
-                {subTitle}
+                {subtitle}
             </div>
         </div>
     );
