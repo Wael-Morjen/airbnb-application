0e78ec1638582ff3f6333d055ff44c4ab72317d2
wael-morjen <waelmorjen@gmail.com>
2023-06-03 00:46:59 +0100

    Fetching listings

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,9 @@ export default async function RootLayout({
           <LoginModal/>
           <Navbar currentUser={currentUser}/>
         </ClientOnly>
+        <div className="pb-20 pt-28">
         {children}
+        </div>
       </body>
     </html>
   )
