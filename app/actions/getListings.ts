2545a483850672b32f7901f1ba32a441cd18b20c
wael-morjen <waelmorjen@gmail.com>
2023-06-12 17:07:41 +0100

    Search modal & filters

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,6 +2,13 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
     userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationString?: string;
+    category?: string;
 }
 
 
@@ -9,12 +16,74 @@ export default async function getListings(
     params: IListingsParams,
 ) {
     try {
-        const { userId } = params
+        const { 
+            userId,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationString,
+            category
+        } = params
 
         let query: any = {};
 
         if (userId) {
             query.userId = userId;
+        }
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (guestCount) {
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if (locationString) {
+            query.locationString = locationString;
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: {
+                                    gte: startDate
+                                },
+                                startDate: {
+                                    lte: startDate
+                                }
+                            },
+                            {
+                                startDate: {
+                                    lte: endDate
+                                },
+                                endDate: {
+                                    gte: endDate
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
         }
 
         const listings = await prisma.listing.findMany({
